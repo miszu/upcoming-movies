8ec4872eb8ac1971abf4f306a27497f505ccf454
Michał Żołnieruk <gierekg@gmail.com>
2017-11-14 00:45:56 +0100

    Changed colors to dark

--- Movies/Movies/Views/UpcomingMoviesPage.xaml.cs
+++ Movies/Movies/Views/UpcomingMoviesPage.xaml.cs
@@ -4,6 +4,8 @@ namespace Movies.Views
 {
     public partial class UpcomingMoviesPage : ContentPage
     {
+        // TODO: rethink background, maybe change it to next backdrops while scrolling?
+        // TODO: remove white selection on iOS
         public UpcomingMoviesPage()
         {
             InitializeComponent();
