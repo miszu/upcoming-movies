e8caea8d87c5ebf17c3552a6eb1d88c64e713bca
Michał Żołnieruk <gierekg@gmail.com>
2017-11-14 00:36:05 +0100

    Added movie details opacity animations

--- Movies/Movies/Views/MovieDetailsPage.xaml.cs
+++ Movies/Movies/Views/MovieDetailsPage.xaml.cs
@@ -1,12 +1,35 @@
-﻿using Xamarin.Forms;
+﻿using System;
+using Xamarin.Forms;
 
 namespace Movies.Views
 {
     public partial class MovieDetailsPage : ContentPage
     {
+        private const int PosterAnimationTime = 400;
+        private const double BackdropTargetOpacity = 0.7;
+
         public MovieDetailsPage()
         {
             InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            BeginOpacityAnimations();
+            BeginTranslationAnimations();
+        }
+
+        private void BeginOpacityAnimations()
+        {
+            PosterImage.FadeTo(1, PosterAnimationTime);
+            DetailsContainer.FadeTo(1, (uint) (2 * PosterAnimationTime));
+            BackdropMask.FadeTo(BackdropTargetOpacity, 5 * PosterAnimationTime);
+        }
+
+        private void BeginTranslationAnimations()
+        {
+            // TODO
         }
     }
 }
