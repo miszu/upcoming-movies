aea4cca3f08be93e2fad43e0710783f4668f80e3
Michał Żołnieruk <gierekg@gmail.com>
2017-11-14 23:52:49 +0100

    Added horizontal movie details page variant

--- Movies/Movies/Views/MovieDetailsPage.xaml.cs
+++ Movies/Movies/Views/MovieDetailsPage.xaml.cs
@@ -6,8 +6,10 @@ namespace Movies.Views
 {
     public partial class MovieDetailsPage : ContentPage
     {
-        private const uint PosterAnimationTime = 600;
-        private const double BackdropTargetOpacity = 0.7;
+        const uint PosterAnimationTime = 600;
+        const double BackdropTargetOpacity = 0.7;
+        double _width;
+        double _height;
 
         public MovieDetailsPage()
         {
@@ -24,13 +26,48 @@ namespace Movies.Views
         {
             await PosterImage.FadeTo(1, PosterAnimationTime);
             DetailsContainer.FadeTo(1, 2 * PosterAnimationTime);
-            IconDetailsContainer.FadeTo(1, 2 * PosterAnimationTime);
             BackdropMask.FadeTo(BackdropTargetOpacity, 5 * PosterAnimationTime);
         }
 
-        private void BeginTranslationAnimations()
+        protected override void OnSizeAllocated(double width, double height)
         {
-            // TODO
+            base.OnSizeAllocated(width, height);
+            if (width != _width || height != _height)
+            {
+                _width = width;
+                _height = height;
+
+                if (width > height)
+                {
+                    MainContainer.RowDefinitions.Clear();
+                    MainContainer.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(2, GridUnitType.Star) });
+                    MainContainer.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(3, GridUnitType.Star) });
+
+                    MainContainer.Children.Remove(DetailsContainer);
+                    MainContainer.Children.Add(DetailsContainer, 1, 0);
+
+                    DetailsContainer.Margin = new Thickness(0, 0, 25, 25);
+                    Grid.SetRowSpan(BackdropImage, 1);
+                    Grid.SetRowSpan(BackdropMask, 1);
+                    Grid.SetColumnSpan(BackdropImage, 2);
+                    Grid.SetColumnSpan(BackdropMask, 2);
+                }
+                else
+                {
+                    MainContainer.ColumnDefinitions.Clear();
+                    MainContainer.RowDefinitions.Add(new RowDefinition { Height = new GridLength(2, GridUnitType.Star) });
+                    MainContainer.RowDefinitions.Add(new RowDefinition { Height = new GridLength(3, GridUnitType.Star) });
+
+                    MainContainer.Children.Remove(DetailsContainer);
+                    MainContainer.Children.Add(DetailsContainer, 0, 1);
+
+                    DetailsContainer.Margin = new Thickness(10, 30, 10, 10);
+                    Grid.SetRowSpan(BackdropImage, 2);
+                    Grid.SetRowSpan(BackdropMask, 2);
+                    Grid.SetColumnSpan(BackdropImage, 1);
+                    Grid.SetColumnSpan(BackdropMask, 1);
+                }
+            }
         }
     }
 }
